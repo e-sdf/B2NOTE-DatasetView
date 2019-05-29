02983c0fc61601bb89502260c6dec9a097f7763a
TomasKulhanek <tkulhanek@esciencefactory.com>
2019-05-29 11:19:32 +0100

    UPDATE: body schema anyof string or struct, corresponding unittests

--- backend/starteve.py
+++ backend/starteve.py
@@ -1,39 +1,97 @@
 from eve import Eve
 
-annotation_schema= {
-  '@context' : { 'type' : 'string' },
-  'id' : { 'anyof':[
-    {'type' : 'string'},
-    {'type':'list', 'schema': { 'type': 'string'}}
-    ]},
-  'body': { 'anyof' : [
-    {'type':'string'},
-    {'type':'list', 'schema': { 'type': 'string'}}
-   ]},
-  'bodyValue': {'type':'string'},
-  'target':{'required':True,'anyof':[
-    {'type':'string'},
-    {'type':'list', 'schema': { 'type': 'string'}}
-  ]}
-}
 
-my_settings = {
+def starteve():
+  string_or_list_of_strings = {'anyof':[
+        {'type':'string'},#single string
+        {'type':'list','schema':{'type':'string'}}# or list of strings
+      ]}
+  body_schema = {'anyof': [
+      {'type': 'string'}, #single string
+      {'type': 'list', 'schema': { # or list of
+        'anyof': [
+          {'type':'string'}, # single strings
+          { 'schema':{# or structure
+      'id': {'type': 'string'},
+      'format': string_or_list_of_strings
+        }}]
+      #'value': {'type': 'string'},
+      #'purpose': {'type': 'string'},
+      #'type': {
+      #  'type': 'list',
+      #  'schema': {
+      #    'type': 'string',
+      #  },
+      #},
+      #'items': {
+      #  'type': 'list',
+      #  'schema': {
+      #    'type': {'type': 'string'},
+      #    'source': {'type': 'string'},
+      #    'value': {'type': 'string'}
+      #  },
+      #},
+      # 'language' : {
+      #    'type' : 'list',
+      #    'schema' : {
+      #        'type' : 'string',
+      #        },
+      #    },
+      # 'format' : {
+      #    'type' : 'list',
+      #    'schema' : {
+      #        'type' : 'string',
+      #        },
+      #    },
+      # 'processingLanguage' : { 'type' : 'string' },
+      # 'textDirection' : { 'type' : 'string' },
+      # 'creator' : {
+      #    'type' : 'list',
+      #    'schema' : agent,
+      #    },
+      # 'created' : { 'type' : 'datetime' },
+      # 'modified' : { 'type' : 'datetime' },
+    }
+       }
+    ]
+  }
+
+
+  annotation_schema = {
+    '@context': {'type': 'string'},
+    'id': {'anyof': [
+      {'type': 'string'},
+      {'type': 'list', 'schema': {'type': 'string'}}
+    ]},
+    'body': body_schema,
+    'bodyValue': {'type': 'string'},
+    'target': {'required': True, 'anyof': [
+      {'type': 'string'},
+      {'type': 'list', 'schema': {'type': 'string'}}
+    ]}
+  }
+  my_settings = {
     'MONGO_HOST': 'localhost',
     'MONGO_PORT': 27017,
     'MONGO_DBNAME': 'b2notedb',
     'MONGO_USERNAME': 'b2note',
     'MONGO_PASSWORD': 'b2note',
-    'DOMAIN': {'annotations': {'allow_unknown':True, 'schema':annotation_schema}},
+    'DOMAIN': {'annotations': {'allow_unknown': True, 'schema': annotation_schema}},
     'RESOURCE_METHODS': ['GET', 'POST', 'DELETE'],
     'ITEM_METHODS': ['GET', 'PATCH', 'PUT', 'DELETE'],
-    'XML' : False
-}
-#'allow_unknown':True, 'schema': {
-#  'context': {'type': 'string'},
-#  'id': {'type': 'string'},
-#  'type': {'type': 'string'},
-#  'body': {'type': 'string'},
-#  'target': {'type': 'string'}
-#}},
-app = Eve(settings=my_settings)
-app.run()
+    'XML': False
+  }
+  # 'allow_unknown':True, 'schema': {
+  #  'context': {'type': 'string'},
+  #  'id': {'type': 'string'},
+  #  'type': {'type': 'string'},
+  #  'body': {'type': 'string'},
+  #  'target': {'type': 'string'}
+  # }},
+  app = Eve(settings=my_settings)
+  app.run()
+
+if __name__== "__main__":
+  print('Instantiating standalone server.')
+  starteve()
+
--- backend/tests.py
+++ backend/tests.py
@@ -1,23 +1,55 @@
-import os
 import unittest
 import requests
+import json
+
+LOCALHOST_ANNOTATIONS = 'http://localhost:5000/annotations'
 
 
+class B2noteRestApiTestCase(unittest.TestCase):
 
-class FlaskrTestCase(unittest.TestCase):
-
-    def setUp(self):
+    evethread = None  # type: None
+    #check whether EVE server is accepting request by HEAD method, if not, start as separate thread
+    @classmethod
+    def setUpClass(self):
       self.headers = {
         'Content-Type': 'application/json',
       }
+      # test whether eve server is up and running
+      print('Setup ')
+      try:
+        response = self.head(self,'')
+        print(response.status_code)
+      except Exception as e:
+        #print(e)
+        print('Starting EVE server in separate thread ...')
+        # code 200 is not returned, then start the eve server
+        import starteve
+        import multiprocessing
+        import time
+        self.evethread = multiprocessing.Process(target=starteve.starteve)
+        self.evethread.start()
+        time.sleep(2)
+        print('Started EVE server ...')
 
-    def tearDown(self):
+
+    #if server was started as separate thread, then terminate it after all tests
+    @classmethod
+    def tearDownClass(self):
       self.headers = {}
-      # some code here
+      # if the eve was started - then it will be stopped when the test process will terminate
+      if self.evethread:
+        self.evethread.terminate()
+        print('Stopped EVE server ...')
 
+
+    def head(self,resource):
+      return requests.head(LOCALHOST_ANNOTATIONS+'/'+resource,headers=self.headers)
+
+    def get(self,resource):
+      return requests.get(LOCALHOST_ANNOTATIONS + '/' + resource, headers=self.headers)
 
     def post(self,data):
-      return requests.post('http://localhost:5000/annotations', headers=self.headers, data=data)
+      return requests.post(LOCALHOST_ANNOTATIONS, headers=self.headers, data=data)
 
 
     def test_create_annotation(self):
@@ -38,6 +70,52 @@ class FlaskrTestCase(unittest.TestCase):
       response = self.post(data)
       #print(response.content)
       assert b'Insertion failure' in response.content
+
+    def test_bad_body_as_integer(self):
+      data = '{"id":"1","body":3}'
+      response = self.post(data)
+      #print(response.content)
+      assert b'Insertion failure' in response.content
+
+    def test_create_annotation_get_annotation_detail(self):
+      data = '{"id":"http://example.org/anno1","body":"http://example.com/post1","target":"http://example.com/page1"}'
+      response = self.post(data)
+      #print(response.content)
+      responsedata = json.loads(response.content)
+      #print(responsedata)
+      assert b'"_status": "OK"' in response.content
+      #print(response.content)
+      self.assertIsInstance(responsedata['_id'], str)
+      response2 = self.get(responsedata['_id'])
+      responsedata2 = json.loads(response2.content)
+      #print(responsedata2)
+      self.assertEquals(responsedata['_id'],responsedata2['_id'])
+
+    def test_stringbody_schema(self):
+      body = '{"id":"http://example.org/anno1","body":"http://example.com/post1","target":"http://example.com/page1"}'
+      response = self.post(body)
+      #print(response.content)
+      assert b'"_status": "OK"' in response.content
+
+    def test_listbody_schema(self):
+
+      body = '{"id":"http://example.org/anno1","body":["http://example.com/post1","http://example.com/post2"],"target":"http://example.com/page1"}'
+      response = self.post(body)
+      #print(response.content)
+      assert b'"_status": "OK"' in response.content
+
+    def test_listbodyids_schema(self):
+      body = '{"id":"http://example.org/anno1","body":[{"id":"http://example.com/post1"},{"id":"http://example.com/post2"}],"target":"http://example.com/page1"}'
+      response = self.post(body)
+      print(response.content)
+      assert b'"_status": "OK"' in response.content
+
+    def test_listbodyformats_schema(self):
+      body = '{"id":"http://example.org/anno1","body":[{"id":"http://example.com/post1","format":"audio/mpeg"},{"id":"http://example.com/post2","format":"application/pdf"}],"target":"http://example.com/page1"}'
+      response = self.post(body)
+      assert b'"_status": "OK"' in response.content
+
+
 
 
 if __name__ == '__main__':
